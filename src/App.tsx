3d142f754d2304065486d61d889c576c8451b57d
KatiuscaH <katiusca.herrera@gmail.com>
2024-07-30 13:47:37 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Travel cost calculation</h1>
+      <div><
+    </>
+  )
+}
+
+export default App
